badf01cd16a5cbd4fdea9d1f967f966baff3fa05
rabiuddin <rabiuddin1@gmail.com>
2025-08-31 22:28:55 +0500

    Refactor API route registration and enhance root endpoint handling

--- src/api.py
+++ src/api.py
@@ -2,6 +2,4 @@
 from fastapi import FastAPI
 
 def register_routes(app: FastAPI):
-    @app.get("/")
-    def read_root():
-        return {"message": "Hello world"}
+    return
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .config.db import engine, Base, DbSession
 from .api import register_routes
 from .logging import configure_logging, LogLevels
-
+from .rate_limiter import limiter
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 
 configure_logging(LogLevels.info)
 
 app = FastAPI()
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 """ Only uncomment below to create new tables, 
 otherwise the tests will fail if not connected
@@ -14,3 +19,7 @@ otherwise the tests will fail if not connected
 # Base.metadata.create_all(bind=engine)
 
 register_routes(app)
+
+@app.get("/")
+async def read_root(request: Request):
+    return {"message": "Hello world"}
